b5f0c7e0b8e3611c246e12303d246d3c8de8a20d
Orphanlol <83834491+Orphanlol@users.noreply.github.com>
2024-01-24 20:37:38 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,5 @@
+(function() {
+  var script = document.createElement('script');
+  script.src = 'https://raw.githubusercontent.com/Orphanlol/ixl-real/main/fartballs.js';
+  document.body.appendChild(script);
+})();
